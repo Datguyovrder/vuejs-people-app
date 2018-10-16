a1145094043f27a0688d1cdd28428f2ef1610fa3
Richard Hui <rhui@hawk.iit.edu>
2018-10-16 03:08:09 -0500

    vuejs people app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Vue.js People App</h1>
+    <div>
+      Name: <input v-model="newPerson.name">
+      Bio: <input v-model="newPerson.bio">
+      <button v-onLclick="addPerson()">Add Person</button>
+    </div>
+
+    <div v-for="person in people" >
+      <h2>{{ person.name }}</h2>
+      <p>{{ person.bio }}</p>
+    </div>
+
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+<style>
+  body {
+    background-color: lightblue;
   }
-}
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      people: [
+      {
+        name: "Kamina",
+        bio: "From Gurren Laggan",
+        bioVisible: true
+      },
+      {
+        name: "Naruto",
+        bio: "From Naruto",
+        bioVisible: true
+      },
+      {
+        name: "Ichigo",
+        bio: "From Bleach",
+        bioVisible: true
+      }
+    ],
+    newPerson: {name: "", bio: "", bioVisible: true}
+  };
+},
+  created: function() {},
+  methods: {
+    addPerson: function() {
+      
+    }
+  }, computed: {}
+};
 </script>
