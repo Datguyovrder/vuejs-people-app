0f74ce2bce6265d3b06c0a46e5ca002788340915
Richard Hui <rhui@hawk.iit.edu>
2018-10-16 21:20:07 -0500

    works with backend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,77 +1,66 @@
 <template>
   <div class="home">
-    <h1>Vue.js People App</h1>
+    <h1>Interesting People</h1>
+
     <div>
-      Name: <input v-model="newPerson.name">
+      Name: <input v-model="newPerson.name">  
       Bio: <input v-model="newPerson.bio">
       <button v-on:click="addPerson()">Add Person</button>
     </div>
 
     <div>
-      <h4>Number of people: {{ people.length }}</h4>
+      <h4>Total Number of People: {{ people.length }}</h4>
     </div>
-    
-    <div v-for="person in people" >
+
+    <div v-for="person in people">
       <h2 @click="toggleBio(person)">{{ person.name }}</h2>
-      <div v-bind:class="{strike: person.bioVisible}">
+      <div v-if="person.bioVisible">
         <h3>{{ person.bio }}</h3>
-        <button @click="removePerson(person)">Remove Person</button>
+        <button @click="deletePerson(person)">Delete</button>
       </div>
-    </div>    
+    </div>
   </div>
 </template>
 
 <style>
-  body {
-    background-color: lightblue;
-  }
-
-  .strike {
-    text-decoration: line-through;
-  }
+.strike {
+  text-decoration: line-through;
+}
 </style>
 
 <script>
+var axios = require('axios');
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
-      people: [
-        {
-          name: "Kamina",
-          bio: "From Gurren Laggan",
-          bioVisible: true
-        },
-        {
-          name: "Naruto",
-          bio: "From Naruto",
-          bioVisible: true
-        },
-        {
-          name: "Ichigo",
-          bio: "From Bleach",
-          bioVisible: true
-        }
-      ],
+      people: [],
       newPerson: {name: "", bio: "", bioVisible: true}
     };
   },
-  created: function() {},
+  created: function() {
+    // console.log(this);
+    axios
+    .get("http://localhost:3000/api/people")
+    .then(function(response) {
+      this.people = response.data;
+      // console.log(this);
+    }.bind(this));
+  },
   methods: {
+    toggleBio: function(inputPerson) {
+      inputPerson.bioVisible = !inputPerson.bioVisible;
+    },
     addPerson: function() {
       if (this.newPerson.name && this.newPerson.bio) {
-        this.people.push(this.newPerson)
-        this.newPerson = {name: "", bio: "", bioVisible:true}
+        this.people.push(this.newPerson);
+        this.newPerson = {name: "", bio: "", bioVisible: true};
       }
     },
-    toggleBio: function(inputPerson) {
-        inputPerson.bioVisible = !inputPerson.bioVisible
-    },
-    removePerson: function(inputPerson) {
+    deletePerson: function(inputPerson) {
       var index = this.people.indexOf(inputPerson);
-      this.people.splice(index, 1)
+      this.people.splice(index, 1);
     }
-  }, 
+  },
   computed: {}
 };
 </script>
