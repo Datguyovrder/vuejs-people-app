c25ae2412aa4225c3a9b2a8a9a89e03064c144f8
Richard Hui <rhui@hawk.iit.edu>
2018-10-22 19:55:19 -0500

    filter and sorting button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="home">
     <h1>Interesting People</h1>
-
     <ul>
       <li v-for="error in errors">{{ error }}</li>
     </ul>
-
     <div>
       Name: <input v-model="newPerson.name">  
       Bio: <input v-model="newPerson.bio">
@@ -16,7 +14,19 @@
       <h4>Total Number of People: {{ people.length }}</h4>
     </div>
 
-    <div v-for="person in people">
+    <div>
+      Search: <input v-model="nameFilter" list="names">
+      <datalist id="names">
+        <option v-for="person in people">{{ person.name }}</option>
+      </datalist>
+    </div>
+
+    <div>
+      <button @click="setSortAttribute('name')">Sort by Name</button>
+      <button @click="setSortAttribute('bio')">Sort by Bio</button>
+    </div>
+
+    <div v-for="person in orderBy(filterBy(people, nameFilter, 'name'), sortAttribute, sortOrder)">
       <h2 @click="toggleBio(person)">{{ person.name }}</h2>
       <div v-if="person.bioVisible">
         <h3>{{ person.bio }}</h3>
@@ -38,17 +48,18 @@ export default {
   data: function() {
     return {
       people: [],
-      newPerson: {name: "", bio: "", bioVisible: true},
-      errors: []
+      newPerson: {name: "", bio: "", bioVisible: false},
+      errors: [],
+      nameFilter: "",
+      sortAttribute: 'name',
+      sortOrder: 1
     };
   },
   created: function() {
-    // console.log(this);
     axios
     .get("http://localhost:3000/api/people")
     .then(response => {
       this.people = response.data;
-      // console.log(this);
     });
   },
   methods: {
@@ -57,30 +68,31 @@ export default {
     },
     addPerson: function() {
       this.errors = [];
-
       var params = {
                     name: this.newPerson.name,
                     bio: this.newPerson.bio
                     };
-
       axios
       .post("http://localhost:3000/api/people", params)
       .then(response => {
-        // console.log(response.data)
         this.people.push(response.data);
         this.newPerson = {name: "", bio: "", bioVisible: false};
       })
       .catch(error => {
-        console.log(error.response.data.errors);
-        this.error = error.response.data.errors;
+        this.errors = error.response.data.errors;
       });
-
-      
-      
     },
     deletePerson: function(inputPerson) {
       var index = this.people.indexOf(inputPerson);
       this.people.splice(index, 1);
+    },
+    setSortAttribute: function(inputAttribute) {
+      if (this.sortAttribute === inputAttribute) {
+        this.sortOrder = this.sortOrder * -1;
+      } else {
+        this.sortOrder = 1;
+      }
+      this.sortAttribute = inputAttribute;
     }
   },
   computed: {}
